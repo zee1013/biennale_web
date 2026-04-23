a0f5645c1e526ebd2256f2bc4df28cd9184f3844
zee1013 <dmswl1251@gmail.com>
2026-04-23 18:09:26 +0900

    갤러리 네비게이션 추가, 전시 스와이퍼

--- js/swiper.js
+++ js/swiper.js
@@ -1,18 +1,45 @@
-window.addEventListener("load", ()=>{
-     const swiper = new Swiper(".main-swiper", {
-        effect : "fade",
-        loop : true,
-        speed : 1700,
-        autoplay:{
-            delay : 2500,
-            disableOnInteraction:false,
-        }
-     });
-     // 갤러리 스와이퍼
-     const gallerySwiper = new Swiper(".gallery-swiper", {
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+window.addEventListener("load", () => {
+  const swiper = new Swiper(".main-swiper", {
+    effect: "fade",
+    loop: true,
+    speed: 1700,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
+  // 갤러리 스와이퍼
+  const gallerySwiper = new Swiper(".gallery-swiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".sw-visual-next",
+      prevEl: ".sw-visual-prev",
+    },
+  });
+  // 전시 스와이퍼
+  const exhibitionSwiper = new Swiper(".exhibition-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 15,
+   //   loop: true,
+   //  grid: {
+   //    rows: 1,
+   //    fill: "row",
+   //  },
+    breakpoints: {
+      480: {
+        spaceBetween: 26,
+        slidesPerView: 2,
+        slidesPerGroup: 3,
       },
-     })
-})
+      768: {
+        spaceBetween: 26,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 2,
+          fill: "row",
+        },
+      },
+    },
+  });
+});
